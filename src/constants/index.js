94198046fdd1fdc10efa78aa49d45e7b266a2e62
Rommel Manalo <manalo.rommel@gmail.com>
2022-07-01 12:38:52 +0800

    clean up hte project stared new one

--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-import images from "./images";
-import { skills, experiences, abouts } from "./dummy";
-
-export { images, skills, experiences, abouts };
--- src/wrapper/index.js
+++
@@ -1,5 +0,0 @@
-// export {default as AppWrap} from './AppWrap';
-import AppWrap from "./AppWrap";
-import MotionWrap from "./MotionWrap";
-
-export { AppWrap, MotionWrap };
